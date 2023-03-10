5cfaf4d71fd2f32e6a12542b5f43586b8318e080
Christina Lujan <christina.lujan89@gmail.com>
2023-03-09 19:38:35 -0800

    many mistakes

--- Week6War.js
+++ Week6War.js
@@ -19,14 +19,20 @@ class Deck {
         this.newDeck();
     }
     newDeck() {
-        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+        const suits = ['H', 'S', 'D', 'C'];
         const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
         for (let i = 0; i < suits.length; i++) {
             console.log(suits[i]);
-            for (let i = 0; i < ranks.length; i++) {
-                const card = new Card(ranks, suits);
+            for (let j = 0; j < ranks.length; j++) {
+                const card = new Card(ranks[j], suits[i]);
                 this.cards.push(card);
             }
+        }
+    }
+    shuffle() {
+        for (let i = this.cards.length -1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
         }
     }
 }
@@ -36,7 +42,7 @@ class Deck {
     //current score
     //add play method
 
-    class Player {
+class Player {
         constructor(name) {
             this.name = name;
             this.hand - []
@@ -54,16 +60,43 @@ class Deck {
     //iterates through a loop where each player plays a card
     //compare ranks of cards and award point for higher
     //if cards are = then both players get 0
-const deck = new Deck();
-deck.shuffle();
 
-const player1 = new Player("Player 1");
-const player2 = new Player("Player 2");
-
-// Deal cards evenly between the two players
-for (let i = 0; i < 26; i++) {
-  player1.hand.push(deck.cards[i]);
-  player2.hand.push(deck.cards[i + 26]);
+class Game {
+    constructor(){
+        this.deck = new Deck();
+        this.deck.shuffle();
+        this.player1 = new Player('Player 1');
+        this.player2 = new Player('Player 2');
+        this.dealCards();
+    }
+    dealCards() {
+        for (let i = 0; i < 26; i++) {
+            this.player1.hand.push(this.deck.cards[i]);
+            this.player2.hand.push(this.deck.cards[i + 26]);
+        }
+    }
+    playRound() {
+        const card1 = this.player1.playCard();
+        const card2 = this.player2.playCard();
+        if (card1.rank > card2.rank) {
+            this.player1.score++;
+        } else if (card2.rank > card1.rank) {
+            this.player2.score++;
+        }
+    }
+    playGame() {
+        for (let i = 0; i < 26; i++) {
+            this.playRound();
+        }
+        if (this.player1.score > this.player2.score) {
+            console.log ('Player 1 wins');
+        } else if (this.player2.score > this.player1.score) {
+            console.log('Player 2 wins!');
+        } else {
+            console.log('Tie!');
+        }
+    }
 }
 
-//display final score and declare winner
+const game = new Game();
+game.playGame();
